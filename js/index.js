134a1414c6d9725c3374b2438ff057f624c3e31b
syzygy333 <brianjonashogue@gmail.com>
2016-05-25 11:27:22 -0400

    geolocation reverse lookup partially working

--- js/index.js
+++ js/index.js
@@ -1,43 +1,27 @@
 // b624c5bdec968d79b2bf1a9e507ab9c8
 
-// Goog: AIzaSyA262NUgtdnuhCsrzK1WxN7I2PrKEEaxT8
+// works in Safari on laptop, not in Chrome on laptop or in Codepen
 
-// $(document).ready(function() {
-//   function getLocation() {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(function() {
-//         $("#location").innerHTML("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
-//       });
-//     } else {
-//       $("#location").innerHTML("Geolocation is not supported by this browser.");
-//     }
-//   }
-//   getLocation();
-// });
-
-function geoFindMe() {
-  var output = document.getElementById("location");
-
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
-
+$(document).ready(function() {
   function success(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
-
-    output.innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
-
-  };
-
+    $.ajax({
+      url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&sensor=true",
+      success: function(data) {
+        var city = data.results[0].address_components[3].long_name;
+        var state = data.results[0].address_components[5].short_name;
+        var country = data.results[0].address_components[6].short_name;
+        $("#location").html(city + ", " + state + ", " + country);
+      }
+    });
+  }
   function showError(error) {
-    output.innerHTML = "Unable to retrieve your location";
-    console.log(error.message);
-  };
-
-  output.innerHTML = "Locating…";
-
-  navigator.geolocation.getCurrentPosition(success, showError);
-}
-geoFindMe();
+    $("#location").html(error.message);
+  }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, showError);
+  } else {
+    $("#location").html("Geolocation is not supported by this browser.");
+  }
+});
