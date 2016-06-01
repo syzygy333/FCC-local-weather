e8353abc0964d748817fb1cffe69728ebcc38df2
syzygy333 <brianjonashogue@gmail.com>
2016-05-31 22:13:12 -0400

    add switch from F to C

--- js/index.js
+++ js/index.js
@@ -12,9 +12,21 @@ $(document).ready(function() {
         var city = data.results[0].address_components[3].long_name;
         var state = data.results[0].address_components[5].short_name;
         var country = data.results[0].address_components[6].short_name;
+        var zip = data.results[0].address_components[7].long_name;
         $("#location").html(city + ", " + state + ", " + country);
       }
     });
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b624c5bdec968d79b2bf1a9e507ab9c8",
+      success: function(data) {
+        fahrenheit = Math.round((data.main.temp * (9/5)) - 459.67);
+        celsius = Math.round(data.main.temp - 273.15);
+        forecast = data.weather[0].main;
+        $("#temp").html(fahrenheit + ' &deg;<button class="text-button">F</a>');
+        $("#forecast").html(forecast);
+      }
+    });
+    // use above for GET to weather API, plug temp into #temp element,
   }
   function showError(error) {
     $("#location").html(error.message);
@@ -25,3 +37,18 @@ $(document).ready(function() {
     $("#location").html("Geolocation is not supported by this browser.");
   }
 });
+// switches F to C
+if ($("#temp:contains('F')")) {
+  $("#temp").click(fahToCel);
+  function fahToCel() {
+    $("#temp").html(celsius + ' &deg;<button class="text-button">C</a>');
+  }
+}
+// won't switch back
+if ($("#temp:contains('C')")) {
+  $("#temp").click(celToFah);
+  function celToFah() {
+    $("#temp").html(fahrenheit + ' &deg;<button class="text-button">C</a>');
+  }
+}
+// use case statement and forecast keywords to choose #forecast element
